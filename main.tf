2a6c1837b3898ae44220861a4161e4a13e54d5f3
JATINKUMAR1008 <97216852+JATINKUMAR1008@users.noreply.github.com>
2023-07-12 13:03:07 +0530

    initial commit

--- main.tf
+++ main.tf
@@ -1,28 +1,71 @@
 terraform {
-  
-
   required_providers {
-    digitalocean={
-        source = "digitalocean/digitalocean"
-        version = "~> 2.0"
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
     }
   }
 }
 
 provider "digitalocean" {}
 
-resource "digitalocean_ssh_key" "web" {
-  name = "Terraform created SSH key"
-  public_key = file("${path.module}/files/id_rsa.pub")
+resource "digitalocean_ssh_key" "ssh-droplet-key" {
+  name       = "Terraform created SSH key"
+  public_key =  file("${path.module}/files/id_rsa.pub")
 }
 
 resource "digitalocean_droplet" "web" {
-  image = "ubuntu-22-10-x64"
-  name = "web-1"
+  count              = 2
+  image              = "ubuntu-22-10-x64"
+  name               = "web-${count.index}"
+  region             = "nyc1"
+  size               = "s-1vcpu-1gb"
+  private_networking = true
+  monitoring         = true
+  ssh_keys = [
+    digitalocean_ssh_key.ssh-droplet-key.id
+  ]
+  user_data = file("${path.module}/files/user_data.sh")
+}
+
+
+resource "digitalocean_loadbalancer" "web" {
+  name   = "web-lb"
   region = "nyc1"
-  size = "s-1vcpu-1gb"
-  ssh_keys = [ 
-    digitalocean_ssh_key.web.id
-   ]
-   user_data = file("${path.module}/files/user_data.sh")
+  forwarding_rule {
+    entry_port     = 80
+    entry_protocol = "http"
+
+    target_port     = 80
+    target_protocol = "http"
+  }
+  
+ 
+  healthcheck {
+    port     = 22
+    protocol = "tcp"
+  }
+  droplet_ids = digitalocean_droplet.web.*.id
+}
+
+resource "digitalocean_firewall" "web" {
+  name = "web-droplet-firewall"
+  droplet_ids = digitalocean_droplet.web.*.id
+
+  inbound_rule {
+    protocol = "tcp"
+    port_range = "22"
+    
+    source_load_balancer_uids = [digitalocean_loadbalancer.web.id]
+  }
+  inbound_rule {
+    protocol = "tcp"
+    port_range = "80"
+    source_load_balancer_uids = [digitalocean_loadbalancer.web.id]
+  }
+  outbound_rule {
+    protocol = "tcp"
+    port_range = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,7 @@
 output "ip_address" {
-  value = digitalocean_droplet.web.ipv4_address
+  value       = digitalocean_droplet.web.*.ipv4_address
   description = "Public ip address of Droplet"
+}
+output "load_balancer_ip" {
+  value = digitalocean_loadbalancer.web.ip
 }
